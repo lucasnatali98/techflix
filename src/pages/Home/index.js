ee9362b7d392edf7885488fceee9b0a4e426ad12
lucasnatali98 <lucasnatali16@gmail.com>
2020-07-28 12:30:21 -0300

    Slider and Carrosel updates

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Menu from '../../components/Menu';
 import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
-import Carousel from '../../components/Carousel';
+import Carousel from '../../components/Carrossel';
 import Footer from '../../components/Footer';
 
 function Home() {
